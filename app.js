60596376da798bbabdccdd49d865936959b00faa
OscarNavarrolol <129440890+OscarNavarrolol@users.noreply.github.com>
2024-01-18 18:11:09 -0500

    Proyecto finalizado

---
+++ app.js
@@ -0,0 +1,71 @@
+let numeroSecreto = 0;
+let intentos = 0;
+let listanum = [];
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('opcion').value);
+    
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        //El usuario no acertó.
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p','El número secreto es menor');
+        } else {
+            asignarTextoElemento('p','El número secreto es mayor');
+        }
+        intentos++;
+        limpiarCaja();
+    }
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#opcion').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numGenerado = Math.floor(Math.random() * 10) + 1;
+    
+    if (listanum.length == 10) {
+        asignarTextoElemento("p", "Ya ya mijo, reinicie");
+    } else {
+        if (listanum.includes(numGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listanum.push(numGenerado);
+            return numGenerado;
+        }
+    }
+}
+
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Juego del número secreto!');
+    asignarTextoElemento('p',`Indica un número del 1 al 10`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+
+condicionesIniciales();
